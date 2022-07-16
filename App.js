86499e678de9095479cd78a055732823b1dd5af9
Surendra Made <zuren.dra96@gmail.com>
2022-07-16 10:21:50 +0800

    implement basic styling from figma

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react"
 import {Text, View,Image, TextInput, SafeAreaView,StyleSheet} from "react-native"
 import kanalLogo from './assets/images/kanal.png';
+import MacbookImage from './assets/images/macbook.jpeg';
 
 const App = () => {
   return (
       <SafeAreaView>
+        <ItemView/>
         <FirstName />
         <LastName />
         <BmcImage />
@@ -49,6 +51,62 @@ class Profile extends Component {
   }
 }
 
+const ItemView = () => {
+  return (
+    <View style={itemStyle.item}>
+      <Image source={MacbookImage} style={itemStyle.image}></Image>
+      <Text style={itemStyle.productName}>New Macbook Pro 2012</Text>
+      <Text style={itemStyle.productPrice}>Rp. 25.000.000</Text>
+      <Text style={itemStyle.productLocation}>Jakarta Barat</Text>
+      <View style={itemStyle.BuyButton}>
+        <Text style={itemStyle.TextBuyButton}>BELI</Text>
+      </View>
+    </View>
+  )
+}
+
+const itemStyle = StyleSheet.create({
+  item:{
+    alignContent:'center',
+    padding:12,
+    backgroundColor:'#F2F2F2',
+    width:212,
+    borderRadius: 8
+  },
+  image:{
+    width:188,
+    height:107,
+    borderRadius:8
+  }, 
+  productName:{
+    fontSize:14,
+    fontWeight:'bold',
+    marginTop:16
+  },
+  productPrice:{
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginTop: 12,
+    color:'#F2994A',
+  },
+  productLocation: {
+    fontSize: 12,
+    fontWeight: '200',
+    marginTop: 12
+  },
+  BuyButton:{
+    backgroundColor:'#6FCF97',
+    paddingVertical:6,
+    borderRadius:25,
+    marginTop:20
+  },
+  TextBuyButton:{
+    fontSize:14,
+    fontWeight:'600',
+    color:'white',
+    textAlign:'center'
+  }
+})
 
 const style = StyleSheet.create({
   text:{
